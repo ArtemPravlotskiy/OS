9022081f0589d4b37c25f1009522591363c2d34d
Artsiom Praulotski <pravlotskiy.artem24@gmail.com>
2025-03-24 10:09:49 +0300

    Update main.cpp

--- lab01_98/main.cpp
+++ lab01_98/main.cpp
@@ -1,4 +1,32 @@
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+struct employee {
+    int num;
+    char name[21];
+    double hours;
+};
+
+void printBinaryFile(const char* filename) {
+    std::ifstream in(filename, std::ios::binary);
+    if (!in) {
+        std::cerr << "Error opening binary file " << filename << "\n";
+        return;
+    }
+    std::cout << "\nThe contents of the binary file:\n";
+    employee emp;
+    while (in.read(reinterpret_cast<char*>(&emp), sizeof(employee))) {
+        std::cout << "Number: " << emp.num
+                  << ", Name: " << emp.name
+                  << ", Hours: " << emp.hours << "\n";
+    }
+    in.close();
+}
+
+void printReportFile() {
+    
+}
