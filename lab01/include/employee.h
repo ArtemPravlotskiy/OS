26bac0f53aced5f517012d8dbb6b89da21af302c
Artsiom Praulotski <pravlotskiy.artem24@gmail.com>
2025-03-29 18:28:20 +0300

    Update employee.h

--- lab01/include/employee.h
+++ lab01/include/employee.h
@@ -5,10 +5,12 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+//#pragma pack(push, 1)
 struct employee {
     int num;            // id num
     char name[10];      // name
     double hours;       // number of hours worked
 };
+//#pragma pack(pop)
 
 #endif //EMPLOYEE_H
