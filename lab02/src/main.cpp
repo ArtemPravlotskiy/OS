4f01043116462d3b78a7f5bc0d97cbb5450509ab
Artsiom Praulotski <144160855+ArtemPravlotskiy@users.noreply.github.com>
2025-04-12 15:18:24 +0300

    Update main.cpp
    Make ThreadMinMax

--- lab02/src/main.cpp
+++ lab02/src/main.cpp
@@ -1,3 +1,36 @@
 //
 // Created by pravl on 07.04.2025.
 //
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int* array = nullptr;
+int arraySize = 0;
+int Min = 0, Max = 0;
+double avg = 0.0;
+
+DWORD WINAPI ThreadMinMax(LPVOID lpParam) {
+    if (arraySize <= 0) {
+        return 0;
+    }
+
+    Min = array[0];
+    Max = array[0];
+
+    for (int i = 1; i < arraySize; ++i) {
+        if (array[i] < Min) {
+            Min = array[i];
+        }
+        Sleep(7);
+
+        if (array[i] > Max) {
+            Max = array[i];
+        }
+        Sleep(7);
+    }
+
+    return 0;
+}
